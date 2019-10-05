951881e5cd10ef1a5ca2e7ef7717099e5c7911e0
Enrique Galceran <enrique@galceran.net>
2019-10-05 21:12:36 +0200

    Pre-adding parameters

--- peces.py
+++ peces.py
@@ -2,11 +2,13 @@
 import RPi.GPIO as GPIO
 import dht11
 import time
+import os
 from Salida_limpia import mostrarresultados
-
+from parametros import generate_parameter_list
 
 # Define GPIO INPUT model
 GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
 
 # temp and button pins
 temp_pin = 19
@@ -21,36 +23,50 @@ for segment in segments_pins_board:
     GPIO.setup(segment, GPIO.OUT)
     GPIO.output(segment, 0)
 
-# Cambiando 24 por 12 funciona por alguna razon.
-# Creo que esta teniendo interferencia con alguna otra cosa
-
 # digit cathodes pins
 digit_pins_board = (22, 27, 17, 12)
 for digit in digit_pins_board:
     GPIO.setup(digit, GPIO.OUT)
     GPIO.output(digit, 1)
 
-# Numbers, 'E' and 'r' pin layout dictionary
-num = {' ': (0, 0, 0, 0, 0, 0, 0),
-       '0': (1, 1, 1, 1, 1, 1, 0),
-       '1': (0, 1, 1, 0, 0, 0, 0),
-       '2': (1, 1, 0, 1, 1, 0, 1),
-       '3': (1, 1, 1, 1, 0, 0, 1),
-       '4': (0, 1, 1, 0, 0, 1, 1),
-       '5': (1, 0, 1, 1, 0, 1, 1),
-       '6': (1, 0, 1, 1, 1, 1, 1),
-       '7': (1, 1, 1, 0, 0, 0, 0),
-       '8': (1, 1, 1, 1, 1, 1, 1),
-       '9': (1, 1, 1, 1, 0, 1, 1),
-       'E': (1, 0, 0, 1, 1, 1, 1),
-       'r': (0, 0, 0, 0, 1, 0, 1),
-       }
+# Numbers and characters 7 segment layout dictionary
+segment_dictionary = {' ': (0, 0, 0, 0, 0, 0, 0),
+                      '0': (1, 1, 1, 1, 1, 1, 0),
+                      '1': (0, 1, 1, 0, 0, 0, 0),
+                      '2': (1, 1, 0, 1, 1, 0, 1),
+                      '3': (1, 1, 1, 1, 0, 0, 1),
+                      '4': (0, 1, 1, 0, 0, 1, 1),
+                      '5': (1, 0, 1, 1, 0, 1, 1),
+                      '6': (1, 0, 1, 1, 1, 1, 1),
+                      '7': (1, 1, 1, 0, 0, 0, 0),
+                      '8': (1, 1, 1, 1, 1, 1, 1),
+                      '9': (1, 1, 1, 1, 0, 1, 1),
+                      'a': (1, 1, 1, 0, 1, 1, 1),
+                      'b': (0, 0, 1, 1, 1, 1, 1),
+                      'c': (1, 0, 0, 1, 1, 1, 0),
+                      'd': (0, 1, 1, 1, 1, 0, 1),
+                      'e': (1, 0, 0, 1, 1, 1, 1),
+                      'f': (1, 0, 0, 0, 1, 1, 1),
+                      'g': (1, 0, 1, 1, 1, 1, 0),
+                      'h': (0, 1, 1, 0, 1, 1, 1),
+                      'i': (0, 0, 0, 0, 1, 1, 0),
+                      'j': (0, 1, 1, 1, 0, 0, 0),
+                      'l': (0, 0, 0, 1, 1, 1, 0),
+                      'n': (0, 0, 1, 0, 1, 0, 1),
+                      'o': (0, 0, 1, 1, 1, 0, 1),
+                      'p': (1, 1, 0, 0, 1, 1, 1),
+                      'r': (0, 0, 0, 0, 1, 0, 1),
+                      's': (1, 0, 1, 1, 0, 1, 1),
+                      't': (0, 0, 0, 1, 1, 1, 1),
+                      'u': (0, 1, 1, 1, 1, 1, 0),
+                      'y': (0, 1, 1, 1, 0, 1, 1),
+                      }
 
 instance = dht11.DHT11(pin=temp_pin)
-print(instance)
-
 temp_readout_time = 2  # seconds
 t_0 = time.time()
+usb_directory = '/media/pi'
+
 
 
 def read_th(temperature_humidity_old):
@@ -61,7 +77,7 @@ def read_th(temperature_humidity_old):
         return text_output, (readout.temperature, readout.humidity)
     else:
         print('Error in readout!')
-        return str(str(temperature_humidity_old[0]) + str(temperature_humidity_old[1])).rjust(4),\
+        return str(str(temperature_humidity_old[0]) + str(temperature_humidity_old[1])).rjust(4), \
             temperature_humidity_old
 
 
@@ -103,14 +119,19 @@ try:
 
         # Temperature/humidity mode
         if mode == 0:
-            show_screen(str_th, num)
+            show_screen(str_th, segment_dictionary)
 
         # Time mode
         elif mode == 1:
             n = time.ctime()[11:13] + time.ctime()[14:16]
             str_time = str(n).rjust(4)
-            show_screen(str_time, num)
+            show_screen(str_time, segment_dictionary)
+
+        # write
+        t_write_1 = time.time()
+
 
 finally:
+    print('\n\nGoodbye!\n')
     GPIO.cleanup()
     f.close()
