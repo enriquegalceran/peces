f6d183e3501259977fa2af51692c420b84ae3977
Enrique Galceran <enrique@galceran.net>
2019-10-05 14:41:29 +0200

    Limpieza inicial. todavia queda mucho

--- peces/peces.py
+++ peces/peces.py
@@ -2,9 +2,10 @@
 import RPi.GPIO as GPIO
 import dht11
 import time
+
 GPIO.setmode(GPIO.BCM)
 
-## 23 por 16
+# 23 por 16
 
 TEMP = 19
 BOTON = 26
@@ -12,31 +13,31 @@ BOTON = 26
 GPIO.setup(TEMP, GPIO.IN)
 GPIO.setup(BOTON, GPIO.IN)
 
-## 23 por 16
-segments = (11,4,16,8,7,10,18,25)
+# 23 por 16
+segments = (11, 4, 16, 8, 7, 10, 18, 25)
 for segment in segments:
     GPIO.setup(segment, GPIO.OUT)
-    GPIO.output(segment,0)
+    GPIO.output(segment, 0)
 
-## Cambiando 24 por 12 funciona por alguna razon.
-## Creo que esta teniendo interferencia con alguna otra cosa
-digits=(22,27,17,12)
+# Cambiando 24 por 12 funciona por alguna razon.
+# Creo que esta teniendo interferencia con alguna otra cosa
+digits = (22, 27, 17, 12)
 
 for digit in digits:
-    GPIO.setup(digit,GPIO.OUT)
-    GPIO.output(digit,1)
+    GPIO.setup(digit, GPIO.OUT)
+    GPIO.output(digit, 1)
 
-num = {' ':(0,0,0,0,0,0,0),
-       '0':(1,1,1,1,1,1,0),
-       '1':(0,1,1,0,0,0,0),
-       '2':(1,1,0,1,1,0,1),
-       '3':(1,1,1,1,0,0,1),
-       '4':(0,1,1,0,0,1,1),
-       '5':(1,0,1,1,0,1,1),
-       '6':(1,0,1,1,1,1,1),
-       '7':(1,1,1,0,0,0,0),
-       '8':(1,1,1,1,1,1,1),
-       '9':(1,1,1,1,0,1,1)}
+num = {' ': (0, 0, 0, 0, 0, 0, 0),
+       '0': (1, 1, 1, 1, 1, 1, 0),
+       '1': (0, 1, 1, 0, 0, 0, 0),
+       '2': (1, 1, 0, 1, 1, 0, 1),
+       '3': (1, 1, 1, 1, 0, 0, 1),
+       '4': (0, 1, 1, 0, 0, 1, 1),
+       '5': (1, 0, 1, 1, 0, 1, 1),
+       '6': (1, 0, 1, 1, 1, 1, 1),
+       '7': (1, 1, 1, 0, 0, 0, 0),
+       '8': (1, 1, 1, 1, 1, 1, 1),
+       '9': (1, 1, 1, 1, 0, 1, 1)}
 
 instance = dht11.DHT11(pin=TEMP)
 
@@ -51,19 +52,18 @@ modo = 1
 pulsado = 0
 boton = 0
 
-
 f = open("Data.txt", "w+")
 try:
     while True:
-	t_1 = time.time()
-	# tiempo suficiente como para volver a medir temperatura
-	if t_1 - t_0 > frecuencia:
+        t_1 = time.time()
+        # tiempo suficiente como para volver a medir temperatura
+        if t_1 - t_0 > frecuencia:
             t_0 = t_1
-	    result = instance.read()
+            result = instance.read()
             if result.is_valid():
-		tiempo = time.ctime()
-		f.write(tiempo + "\n")
-                n = str(result.temperature) +  str(result.humidity)
+                tiempo = time.ctime()
+                f.write(tiempo + "\n")
+                n = str(result.temperature) + str(result.humidity)
                 s = str(n).rjust(4)
 
         # Cambiar modo con el boton
@@ -73,27 +73,27 @@ try:
             modo = (modo + 1) % 2
             pulsado = 1
         elif boton == 0:
-	    pulsado = 0
+            pulsado = 0
 
-	# Si esta en modo temp/humedad
+        # Si esta en modo temp/humedad
         if modo == 0:
             for digit in range(4):
-                for loop in range(0,7):
-                    GPIO.output(segments[loop],num[s[digit]][loop])                
+                for loop in range(0, 7):
+                    GPIO.output(segments[loop], num[s[digit]][loop])
                 if digit == 1:
-                    GPIO.output(25,1)
+                    GPIO.output(25, 1)
                 else:
-	            GPIO.output(25,0)
-                GPIO.output(digits[digit],0)
+                    GPIO.output(25, 0)
+                GPIO.output(digits[digit], 0)
                 time.sleep(0.005)
-                GPIO.output(digits[digit],1)
+                GPIO.output(digits[digit], 1)
 
-	# Si esta en modo reloj
+        # Si esta en modo reloj
         elif modo == 1:
             n = time.ctime()[11:13] + time.ctime()[14:16]
             s = str(n).rjust(4)
             for digit in range(4):
-                for loop in range(0,7):
+                for loop in range(0, 7):
                     GPIO.output(segments[loop], num[s[digit]][loop])
                     if (int(time.ctime()[18:19]) % 2 == 0) and (digit == 1):
                         GPIO.output(25, 1)
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from setuptools import find_packages
+
+def setup_package():
+
+    from peces import __version__
+    REQUIRES = ['numpy', 'astropy>=1.0', 'pandas', 'matplotlib']
+    META_DATA = dict(
+        name='peces',
+        version=__version__,
+        description='Image reduction for CAFOS instrument in CAHA',
+        author='Enrique Galceran',
+        author_email='egalcera@ucm.es',
+        packages=find_packages('.'),
+        entry_points={
+            'console_scripts': [
+                'peces = peces.ULTRON:main',
+		'peces-version = peces.version:main'
+            ],
+            },
+        setup_requires=['peces'],
+        install_requires=REQUIRES,
+        zip_safe=False
+        )
+
+    setup(**META_DATA)
+
+
+if __name__ == '__main__':
+    setup_package()
